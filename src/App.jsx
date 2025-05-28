4bf29534080b72935093557c2f45e9cf7fb94c95
MuhYafiTaqq <twizz1366@gmail.com>
2025-05-28 10:09:37 +0700

    modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // src/App.jsx
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { AuthProvider } from './auth/authContext'; // Pastikan path ini benar
+import { AuthProvider } from './auth/AuthContext'; // Pastikan path ini benar
 import AppRoutes from './routes/AppRoutes';   // Pastikan path ini benar
 import './App.css';
 
